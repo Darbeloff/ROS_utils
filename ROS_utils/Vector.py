1f17c3e656d39d6764d50f9ddf61a76f01055d1a
Quinn Bowers <q.bowers@outlook.com>
2022-09-01 11:56:24 -0400

    switch to .wxyz quaternions

--- ROS_utils/Vector.py
+++ ROS_utils/Vector.py
@@ -3,15 +3,19 @@ import numpy as np
 
 class Vector:
     """
-    A handy class to convert between iterables and .xyzw datatypes
+    A handy class to convert between iterables and .wxyz datatypes
     """
     def __init__(self, array):
-        self.x = array[0]
-        self.y = array[1]
-        if len(array) > 2:
-            self.z = array[2]
         if len(array) > 3:
-            self.w = array[3]
+            self.w = array[0]
+            w_offset = 1
+        else:
+            w_offset = 0
+        
+        self.x = array[0 + w_offset]
+        self.y = array[1 + w_offset]
+        if len(array) > 2:
+            self.z = array[2 + w_offset]
     
     @staticmethod
     def to_array(vector):
@@ -19,7 +23,8 @@ class Vector:
             return None
             
         if hasattr(vector, 'w'):
-            return np.array([vector.x, vector.y, vector.z, vector.w])
+            # Quaternions are .wxyz
+            return np.array([vector.w, vector.x, vector.y, vector.z])
         elif hasattr(vector, 'z'):
             return np.array([vector.x, vector.y, vector.z])
         else:
